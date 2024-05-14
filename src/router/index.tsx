d4244697f76341021c1e625eccd76a428835d758
PabloSA57 <camusps577@gmail.com>
2024-05-14 15:50:15 -0300

    resolve error4

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -8,10 +8,6 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <PageChat />,
-  },
-  {
-    path: "chat/",
-    element: <PageAuth />,
     errorElement: <p>Error</p>,
     children: [
       {
